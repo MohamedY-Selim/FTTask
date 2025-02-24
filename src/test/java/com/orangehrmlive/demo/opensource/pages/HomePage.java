f162f6871dda4f77a90da8b9708688d7d2ad4e75
Mohamed Yehia Selim Hassan <145566961+MohamedY-Selim@users.noreply.github.com>
2025-02-24 18:26:41 +0200

    Update HomePage.java

--- src/test/java/com/orangehrmlive/demo/opensource/pages/HomePage.java
+++ src/test/java/com/orangehrmlive/demo/opensource/pages/HomePage.java
@@ -4,33 +4,26 @@ import com.orangehrmlive.demo.opensource.base.BasePage;
 import com.orangehrmlive.demo.opensource.factory.EndPoint;
 import com.orangehrmlive.demo.opensource.utils.ConfigUtils;
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage<HomePage> {
 
-    //Constructor
+    // Constructor
     public HomePage(WebDriver driver) {
         super(driver);
     }
-    //
 
-    //Elements
-    @FindBy(xpath = "//h6[text()='Dashboard']")
-    private WebElement homePageHeader;
-    @FindBy(className = "oxd-userdropdown-tab")
-    private WebElement userDropdown;
-    @FindBy(xpath = "//h5[text()='Login']")
-    private WebElement loginPageHeader;
-    @FindBy(xpath = "//a[span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']]")
-    private WebElement adminTab;
-    @FindBy(xpath = "//a[span[contains(@class, 'oxd-main-menu-item--name') and text()='Recruitment']]")
-    private WebElement recruitmentTab;
-    //
+    // Locators
+    private By homePageHeader = By.xpath("//h6[text()='Dashboard']");
+    private By userDropdown = By.className("oxd-userdropdown-tab");
+    private By loginPageHeader = By.xpath("//h5[text()='Login']");
+    private By adminTab = By.xpath("//a[span[contains(@class, 'oxd-main-menu-item--name') and text()='Admin']]");
+    private By recruitmentTab = By.xpath("//a[span[contains(@class, 'oxd-main-menu-item--name') and text()='Recruitment']]");
 
-    //Methods
+    // Methods
     @Step("Load the Home Page")
     @Override
     public HomePage load() {
@@ -40,28 +33,27 @@ public class HomePage extends BasePage<HomePage> {
 
     @Step("Verify that Home Page Header is Displayed")
     public boolean isHomePageHeaderDisplayed() {
-        return homePageHeader.isDisplayed();
+        return driver.findElement(homePageHeader).isDisplayed();
     }
 
     @Step("Verify that User Dropdown is Displayed")
     public boolean isUserDropdownDisplayed() {
         try {
-            return userDropdown.isDisplayed();
+            return driver.findElement(userDropdown).isDisplayed();
         } catch (NoSuchElementException e) {
             return false;
         }
-
     }
 
     @Step("Click on Admin tab on the left side menu")
     public UserManagementPage clickOnAdminTab() {
-        adminTab.click();
+        driver.findElement(adminTab).click();
         return new UserManagementPage(driver);
     }
+
     @Step("Click on Recruitment tab on the left side menu")
     public RecruitmentPage clickOnRecruitmentTab() {
-        recruitmentTab.click();
+        driver.findElement(recruitmentTab).click();
         return new RecruitmentPage(driver);
     }
-
 }
