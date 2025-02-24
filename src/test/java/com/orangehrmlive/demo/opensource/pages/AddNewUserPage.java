8c7b19a2146f80dbda23b1dfbc842749fc4be1bf
Mohamed Yehia Selim Hassan <145566961+MohamedY-Selim@users.noreply.github.com>
2025-02-24 18:30:58 +0200

    Update AddNewUserPage.java

--- src/test/java/com/orangehrmlive/demo/opensource/pages/AddNewUserPage.java
+++ src/test/java/com/orangehrmlive/demo/opensource/pages/AddNewUserPage.java
@@ -9,44 +9,28 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AddNewUserPage extends BasePage<AddNewUserPage> {
-    User user;
+    private User user;
 
-    //Driver
-    WebDriver driver;
-
-    //Constructor
+    // Constructor
     public AddNewUserPage(WebDriver driver) {
         super(driver);
     }
-    //
 
-    //Elements
-    @FindBy(xpath = "//h6[text()='Add User']")
-    private WebElement addUserHeader;
-    @FindBy(xpath = "//label[text()='User Role']/following::div[contains(@class, 'oxd-select-text-input')][1]")
-    private WebElement userRoleDropDown;
-    @FindBy(xpath = "//label[text()='Status']/following::div[contains(@class, 'oxd-select-text-input')][1]")
-    private WebElement statusDropDown;
-    @FindBy(xpath = "//label[text()='Employee Name']/following::div[contains(@class, 'oxd-autocomplete-text-input')][1]/input")
-    private WebElement employeeNameInput;
-    @FindBy(xpath = "//label[text()='Username']/ancestor::div[contains(@class, 'oxd-input-group')]/descendant::input")
-    private WebElement userNameInput;
-    @FindBy(xpath = "//label[text()='Password']/following::input[contains(@class, 'oxd-input')][1]")
-    private WebElement passwordInput;
-    @FindBy(xpath = "//label[text()='Confirm Password']/following::input[contains(@class, 'oxd-input')][1]")
-    private WebElement confirmPasswordInput;
-    @FindBy(xpath = "//button[@type='submit']")
-    private WebElement saveButton;
-    @FindBy(xpath = "//div[@role='listbox']")
-    private WebElement listOfOptions;
+    // Locators
+    private By addUserHeader = By.xpath("//h6[text()='Add User']");
+    private By userRoleDropDown = By.xpath("//label[text()='User Role']/following::div[contains(@class, 'oxd-select-text-input')][1]");
+    private By statusDropDown = By.xpath("//label[text()='Status']/following::div[contains(@class, 'oxd-select-text-input')][1]");
+    private By employeeNameInput = By.xpath("//label[text()='Employee Name']/following::div[contains(@class, 'oxd-autocomplete-text-input')][1]/input");
+    private By userNameInput = By.xpath("//label[text()='Username']/ancestor::div[contains(@class, 'oxd-input-group')]/descendant::input");
+    private By passwordInput = By.xpath("//label[text()='Password']/following::input[contains(@class, 'oxd-input')][1]");
+    private By confirmPasswordInput = By.xpath("//label[text()='Confirm Password']/following::input[contains(@class, 'oxd-input')][1]");
+    private By saveButton = By.xpath("//button[@type='submit']");
+    private By listOfOptions = By.xpath("//div[@role='listbox']");
 
-    //
-
-    //Methods
+    // Methods
     @Step("Load the User Management Page")
     @Override
     public AddNewUserPage load() {
@@ -56,29 +40,30 @@ public class AddNewUserPage extends BasePage<AddNewUserPage> {
 
     @Step("Verify that Add User Form Header is Displayed")
     public boolean isAddUserFormHeaderDisplayed() {
-        return addUserHeader.isDisplayed();
+        return driver.findElement(addUserHeader).isDisplayed();
     }
 
     @Step("Fill New User Data")
     public AddNewUserPage fillNewUserData() {
         user = UserUtils.generateRandomUser();
-        selectFromTheDropDown(userRoleDropDown, user.getUserRole());
-        selectFromTheDropDown(statusDropDown, user.getStatus());
-        employeeNameInput.sendKeys(user.getEmployeeName());
+        selectFromTheDropDown(driver.findElement(userRoleDropDown), user.getUserRole());
+        selectFromTheDropDown(driver.findElement(statusDropDown), user.getStatus());
+        driver.findElement(employeeNameInput).sendKeys(user.getEmployeeName());
+
         explicitWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='listbox']//span[contains(text(), '" + user.getEmployeeName() + "')][1]")));
         selectFromTheDropDownWithKeys();
-        String employeeName = employeeNameInput.getAttribute("value");
+
+        String employeeName = driver.findElement(employeeNameInput).getAttribute("value");
         user.setEmployeeName(employeeName);
-        userNameInput.sendKeys(user.getUserName());
-        passwordInput.sendKeys(user.getPassword());
-        confirmPasswordInput.sendKeys(user.getPassword());
+        driver.findElement(userNameInput).sendKeys(user.getUserName());
+        driver.findElement(passwordInput).sendKeys(user.getPassword());
+        driver.findElement(confirmPasswordInput).sendKeys(user.getPassword());
         return this;
     }
 
     @Step("Click on Save Button")
     public User clickOnSaveButton() {
-        saveButton.click();
+        driver.findElement(saveButton).click();
         return user;
     }
-
 }
