ecdbba461035cbae7bcd94585c06fdf2843d3746
Mohamed Yehia Selim Hassan <145566961+MohamedY-Selim@users.noreply.github.com>
2025-02-24 18:37:34 +0200

    Update UserManagementPage.java

--- src/test/java/com/orangehrmlive/demo/opensource/pages/UserManagementPage.java
+++ src/test/java/com/orangehrmlive/demo/opensource/pages/UserManagementPage.java
@@ -1,14 +1,10 @@
 package com.orangehrmlive.demo.opensource.pages;
 
 import com.orangehrmlive.demo.opensource.base.BasePage;
-import com.orangehrmlive.demo.opensource.factory.EndPoint;
 import com.orangehrmlive.demo.opensource.objects.User;
-import com.orangehrmlive.demo.opensource.utils.ConfigUtils;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class UserManagementPage extends BasePage<UserManagementPage> {
 
@@ -18,60 +14,14 @@ public class UserManagementPage extends BasePage<UserManagementPage> {
     }
 
     // Locators
-    private By userManagementPageHeader = By.xpath("//h6[contains(@class, 'oxd-topbar-header-breadcrumb-level') and text()='User Management']");
-    private By usersCount = By.xpath("//div[@class='orangehrm-horizontal-padding orangehrm-vertical-padding']/span[@class='oxd-text oxd-text--span']");
-    private By addButton = By.xpath("//button[contains(@class, 'oxd-button') and contains(., 'Add')]");
-    private By userNameInput = By.xpath("//label[text()='Username']/following::input[1]");
     private By userRoleDropdown = By.xpath("//label[contains(text(),'User Role')]/following::div[@class='oxd-select-text-input']");
-    private By employeeNameInput = By.xpath("//label[contains(text(),'Employee Name')]/following::input[@placeholder='Type for hints...']");
     private By statusDropdown = By.xpath("//label[contains(text(),'Status')]/following::div[contains(@class,'oxd-select-text-input')]");
-    private By searchButton = By.xpath("//button[@type='submit']");
 
     // Methods
-    @Step("Load the User Management Page")
-    @Override
-    public UserManagementPage load() {
-        driver.get(ConfigUtils.getInstance().getBaseUrl() + EndPoint.USER_MANAGEMENT_PAGE_END_POINT);
-        return this;
-    }
-
-    @Step("Verify that User Management Page Header is Displayed")
-    public boolean isUserManagementPageHeaderDisplayed() {
-        return driver.findElement(userManagementPageHeader).isDisplayed();
-    }
-
-    @Step("Get Current users count")
-    public int getUserCount() {
-        explicitWait().until(ExpectedConditions.visibilityOfElementLocated(usersCount));
-        return Integer.parseInt(driver.findElement(usersCount).getText().replaceAll("[^0-9]", ""));
-    }
-
-    @Step("Click on Add Button")
-    public AddNewUserPage clickOnAddButton() {
-        driver.findElement(addButton).click();
-        return new AddNewUserPage(driver);
-    }
-
     @Step("Fill User Data to search")
     public UserManagementPage fillUserDataToSearch(User user) {
-        driver.findElement(userNameInput).sendKeys(user.getUserName());
-        selectFromTheDropDown(driver.findElement(userRoleDropdown), user.getUserRole());
-        driver.findElement(employeeNameInput).sendKeys(user.getEmployeeName());
-        selectSpecificOptionFromTheDropDown(user.getEmployeeName());
-        selectFromTheDropDown(driver.findElement(statusDropdown), user.getStatus());
-        driver.findElement(searchButton).click();
-        return this;
-    }
-
-    @Step("Delete the new User")
-    public UserManagementPage deleteNewUser(User user) {
-        By deleteButtonLocator = By.xpath("//div[@class='oxd-table-row oxd-table-row--with-border' and .//div[text()='" + user.getUserName() + "']]//button[contains(@class, 'oxd-icon-button') and .//i[contains(@class, 'bi-trash')]]");
-        By confirmDeleteButtonLocator = By.xpath("//div[contains(@class, 'orangehrm-modal-footer')]//button[contains(@class, 'oxd-button--label-danger')]");
-
-        WebElement deleteButton = explicitWait().until(ExpectedConditions.visibilityOfElementLocated(deleteButtonLocator));
-        deleteButton.click();
-        WebElement confirmDeleteButton = explicitWait().until(ExpectedConditions.visibilityOfElementLocated(confirmDeleteButtonLocator));
-        confirmDeleteButton.click();
+        selectFromTheDropDown(userRoleDropdown, user.getUserRole());
+        selectFromTheDropDown(statusDropdown, user.getStatus());
         return this;
     }
 }
