86f8fe59cbabafe10670f9dd876e4bd63394880a
Mohamed Yehia Selim Hassan <145566961+MohamedY-Selim@users.noreply.github.com>
2025-02-24 18:49:18 +0200

    Update RecruitmentPage.java

--- src/test/java/com/orangehrmlive/demo/opensource/pages/RecruitmentPage.java
+++ src/test/java/com/orangehrmlive/demo/opensource/pages/RecruitmentPage.java
@@ -4,71 +4,65 @@ import com.orangehrmlive.demo.opensource.base.BasePage;
 import com.orangehrmlive.demo.opensource.factory.EndPoint;
 import com.orangehrmlive.demo.opensource.objects.User;
 import com.orangehrmlive.demo.opensource.utils.ConfigUtils;
+import com.orangehrmlive.demo.opensource.utils.UserUtils;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class RecruitmentPage extends BasePage<RecruitmentPage> {
+public class AddNewUserPage extends BasePage<AddNewUserPage> {
+    private User user;
 
     // Constructor
-    public RecruitmentPage(WebDriver driver) {
+    public AddNewUserPage(WebDriver driver) {
         super(driver);
     }
 
     // Locators
-    private By recruitmentPageHeader = By.cssSelector("h6.oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module");
-    private By usersCount = By.xpath("//div[@class='orangehrm-horizontal-padding orangehrm-vertical-padding']/span[@class='oxd-text oxd-text--span']");
-    private By addButton = By.xpath("//button[contains(@class, 'oxd-button') and contains(., 'Add')]");
-    private By userNameInput = By.xpath("//label[text()='Username']/following::input[1]");
-    private By userRoleDropdown = By.xpath("//label[contains(text(),'User Role')]/following::div[@class='oxd-select-text-input']");
-    private By employeeNameInput = By.xpath("//label[contains(text(),'Employee Name')]/following::input[@placeholder='Type for hints...']");
-    private By statusDropdown = By.xpath("//label[contains(text(),'Status')]/following::div[contains(@class,'oxd-select-text-input')]");
-    private By searchButton = By.xpath("//button[@type='submit']");
+    private By addUserHeader = By.xpath("//h6[text()='Add User']");
+    private By userRoleDropDown = By.xpath("//label[text()='User Role']/following::div[contains(@class, 'oxd-select-text-input')][1]");
+    private By statusDropDown = By.xpath("//label[text()='Status']/following::div[contains(@class, 'oxd-select-text-input')][1]");
+    private By employeeNameInput = By.xpath("//label[text()='Employee Name']/following::div[contains(@class, 'oxd-autocomplete-text-input')][1]/input");
+    private By userNameInput = By.xpath("//label[text()='Username']/ancestor::div[contains(@class, 'oxd-input-group')]/descendant::input");
+    private By passwordInput = By.xpath("//label[text()='Password']/following::input[contains(@class, 'oxd-input')][1]");
+    private By confirmPasswordInput = By.xpath("//label[text()='Confirm Password']/following::input[contains(@class, 'oxd-input')][1]");
+    private By saveButton = By.xpath("//button[@type='submit']");
+    private By listOfOptions = By.xpath("//div[@role='listbox']");
 
     // Methods
-    @Step("Load the Recruitment Page")
+    @Step("Load the User Management Page")
     @Override
-    public RecruitmentPage load() {
-        driver.get(ConfigUtils.getInstance().getBaseUrl() + EndPoint.Recruitment_PAGE_END_POINT);
+    public AddNewUserPage load() {
+        driver.get(ConfigUtils.getInstance().getBaseUrl() + EndPoint.HOME_PAGE_END_POINT);
         return this;
     }
 
-    @Step("Verify that Recruitment Header is Displayed")
-    public boolean isRecruitmentPageHeaderDisplayed() {
-        return driver.findElement(recruitmentPageHeader).isDisplayed();
+    @Step("Verify that Add User Form Header is Displayed")
+    public boolean isAddUserFormHeaderDisplayed() {
+        return driver.findElement(addUserHeader).isDisplayed();
     }
 
-    @Step("Get Current candidates count")
-    public int getCandidatesCount() {
-        explicitWait().until(ExpectedConditions.visibilityOfElementLocated(usersCount));
-        return Integer.parseInt(driver.findElement(usersCount).getText().replaceAll("[^0-9]", ""));
-    }
-
-    @Step("Click on Add Button")
-    public AddNewUserPage clickOnAddButton() {
-        driver.findElement(addButton).click();
-        return new AddNewUserPage(driver);
-    }
-
-    @Step("Fill User Data to search")
-    public RecruitmentPage fillUserDataToSearch(User user) {
-        driver.findElement(userNameInput).sendKeys(user.getUserName());
-        selectFromTheDropDown(userRoleDropdown, user.getUserRole());
+    @Step("Fill New User Data")
+    public AddNewUserPage fillNewUserData() {
+        user = UserUtils.generateRandomUser();
+        selectFromTheDropDown(userRoleDropDown, user.getUserRole());
+        selectFromTheDropDown(statusDropDown, user.getStatus());
         driver.findElement(employeeNameInput).sendKeys(user.getEmployeeName());
-        selectSpecificOptionFromTheDropDown(user.getEmployeeName());
-        selectFromTheDropDown(statusDropdown, user.getStatus());
-        driver.findElement(searchButton).click();
+
+        explicitWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='listbox']//span[contains(text(), '" + user.getEmployeeName() + "')][1]")));
+        selectFromTheDropDownWithKeys();
+
+        String employeeName = driver.findElement(employeeNameInput).getAttribute("value");
+        user.setEmployeeName(employeeName);
+        driver.findElement(userNameInput).sendKeys(user.getUserName());
+        driver.findElement(passwordInput).sendKeys(user.getPassword());
+        driver.findElement(confirmPasswordInput).sendKeys(user.getPassword());
         return this;
     }
 
-    @Step("Delete the new User")
-    public RecruitmentPage deleteNewUser(User user) {
-        By deleteButtonLocator = By.xpath("//div[@class='oxd-table-row oxd-table-row--with-border' and .//div[text()='" + user.getUserName() + "']]//button[contains(@class, 'oxd-icon-button') and .//i[contains(@class, 'bi-trash')]]");
-        By confirmDeleteButtonLocator = By.xpath("//div[contains(@class, 'orangehrm-modal-footer')]//button[contains(@class, 'oxd-button--label-danger')]");
-
-        explicitWait().until(ExpectedConditions.visibilityOfElementLocated(deleteButtonLocator)).click();
-        explicitWait().until(ExpectedConditions.visibilityOfElementLocated(confirmDeleteButtonLocator)).click();
-        return this;
+    @Step("Click on Save Button")
+    public User clickOnSaveButton() {
+        driver.findElement(saveButton).click();
+        return user;
     }
 }
